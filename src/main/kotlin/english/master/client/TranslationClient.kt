a41e8413cabe9fb0977c0f6e4784251eb9ae8061
ElinaBabakovaExtern <Elina.Babakova.extern@solarisbank.de>
2023-04-11 21:30:54 +0300

    add word translation

--- src/main/kotlin/english/master/client/TranslationClient.kt
+++ src/main/kotlin/english/master/client/TranslationClient.kt
@@ -3,7 +3,7 @@ package english.master.client
 import com.fasterxml.jackson.annotation.JsonProperty
 
 class TranslationClient : AbstractClient() {
-    private val url = "http://localhost:3000/api/v1/en/uk"
+    private val url = "https://lingva.ml/api/v1/en/uk"
 
     fun translate(query: String): String {
         return get("${url}/${encode(query)}", Translation::class.java)
